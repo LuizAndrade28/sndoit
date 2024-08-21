65dc29d76679c7e6d35c12a0ba9fcd312adcba44
LuizAndrade <lcfsandrade@gmail.com>
2024-08-21 19:57:16 -0300

    todos and layout - resizing finished

--- app/models/subtodo.rb
+++ app/models/subtodo.rb
@@ -2,7 +2,7 @@ class Subtodo < ApplicationRecord
   belongs_to :todo
   belongs_to :user
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
 
   default_scope -> { order(created_at: :desc) }
 end
